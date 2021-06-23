914f69e84c1519c0a528897c4e937b1b386e615f
gustavolio <g.cdcomp@gmail.com>
2021-06-23 14:34:42 +0000

    Save modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json }, constrants: { subdamin: 'api' }, path: "/" do
-    
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1) do
+      resources :tasks
+    end
+
+    namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2) do
+      resources :tasks
+    end
   end 
 
 end
