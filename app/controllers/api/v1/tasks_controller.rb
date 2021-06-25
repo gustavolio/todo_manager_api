74648a594139880565a92d48c6685eded2821ef0
gustavolio <g.cdcomp@gmail.com>
2021-06-25 22:07:54 +0000

    Add Task Controller

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::TasksController < ApplicationController
+    before_action :authenticate_with_token!
+
+    def index
+        tasks = current_user.tasks
+        render json: {tasks: tasks}
+    end
+
+    def show
+        task = current_user.tasks.find(params[:id])
+        render json: task
+    end
+
+    def create
+        task = current_user.tasks.build(task_params)
+        if task.save
+            render json: task
+        else 
+            render json: { errors: task.errors }
+        end
+    end
+
+    def update
+        task = current_user.tasks.find(params[:id])
+
+        if task.update_attributes(task_params)
+            render json: task
+        else 
+            render json: { errors: task.errors }
+        end
+    end
+    
+    
+    private 
+    def task_params
+        params.require(:task).permit(:title, :description, :status, :visible)
+    end
+end
