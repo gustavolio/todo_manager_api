5a6d498a8d8f19697aaebc4a6358f202f692f4f7
gustavolio <g.cdcomp@gmail.com>
2021-06-25 18:29:03 +0000

    Add Controller and Test Delete Session

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::SessionsController < ApplicationController
-
     def create
         user = User.find_by(email: session_params[:email])
 
@@ -13,8 +12,14 @@ class Api::V1::SessionsController < ApplicationController
         end
     end
 
-    private
+    def destroy
+        user = User.find_by( auth_token: params[:id] )
+        user.generate_authentication_token!
+        user.save
+        head 204
+    end
 
+    private
     def session_params
         params.require(:session).permit(:email, :password)
     end
